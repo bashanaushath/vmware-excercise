1d796202198833ae412afe2dc5b4e469c24f8c95
bashanaushath <101747761+bashanaushath@users.noreply.github.com>
2022-03-29 21:04:51 +0100

    Update server.js
    Replaced the file with new code

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
